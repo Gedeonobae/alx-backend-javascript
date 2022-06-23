99f4b70506299d76a122e0f383c12a3cc700b989
Gedeon Obae Gekonge <85337093+Gedeonobae@users.noreply.github.com>
2022-06-23 07:33:21 +0300

    upload file

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,40 @@
+/** Renders table based on a list of objects */
+
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const studentA: Student = {
+  firstName: 'Kevin',
+  lastName: 'Owino',
+  age: 27,
+  location: 'Migori-Kenya',
+}
+
+const studentB: Student = {
+  firstName: 'Felix',
+  lastName: 'Abongo',
+  age: 26,
+  location: 'Mombasa-Kenya',
+}
+
+const studentList = [studentA, studentB];
+
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+
+studentList.forEach((obj) => {
+  const row = document.createElement('tr');
+  const cellName = document.createElement('td');
+  const cellLocation = document.createElement('td');
+  cellName.textContent = obj.firstName;
+  cellLocation.textContent = obj.location;
+  row.appendChild(cellName);
+  row.appendChild(cellLocation);
+  tbody.appendChild(row);
+});
+table.appendChild(tbody);
+document.body.appendChild(table);
